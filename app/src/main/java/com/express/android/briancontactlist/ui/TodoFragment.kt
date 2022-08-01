5ecc1e038602040e7dc8c57305cf420893a4e832
Brian Donnoe <dunow33@hotmail.com>
2022-07-31 22:00:04 -0400

    updating

--- app/src/main/java/com/express/android/briancontactlist/ui/TodoFragment.kt
+++ app/src/main/java/com/express/android/briancontactlist/ui/TodoFragment.kt
@@ -1,9 +1,15 @@
 package com.express.android.briancontactlist.ui
 
+import android.R
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
@@ -12,6 +18,7 @@ import com.express.android.briancontactlist.databinding.FragmentTodoBinding
 import com.express.android.briancontactlist.db.TodoDatabase
 import com.express.android.briancontactlist.model.Todo
 import com.express.android.briancontactlist.repository.TodoRepository
+
 
 class TodoFragment : Fragment(), TodoAdapter.HandleItemClick {
 
@@ -57,6 +64,27 @@ class TodoFragment : Fragment(), TodoAdapter.HandleItemClick {
                 todoAdapter.differ.submitList(it)
             }
         }
+    }
+
+    private fun showEditTodoDialog() {
+        val dialogBuilder: AlertDialog = Builder(this).create()
+        val dialogView: View = layoutInflater.inflate(R.layout.add_category_layout, null)
+        val enterCategoryInput = dialogView.findViewById<EditText>(R.id.enterCategoryInput)
+        val createButton = dialogView.findViewById<TextView>(R.id.createButton)
+        val cancelButton = dialogView.findViewById<TextView>(R.id.cancelButton)
+        cancelButton.setOnClickListener { dialogBuilder.dismiss() }
+        createButton.setOnClickListener(View.OnClickListener {
+            val name = enterCategoryInput.text.toString()
+            if (TextUtils.isEmpty(name)) {
+                Toast.makeText(this@MainActivity, "Enter category name", Toast.LENGTH_SHORT).show()
+                return@OnClickListener
+            }
+            categoryForEdit.categoryName = name
+            viewModel.updateCategory(categoryForEdit)
+            dialogBuilder.dismiss()
+        })
+        dialogBuilder.setView(dialogView)
+        dialogBuilder.show()
     }
 
     override fun itemClick(todo: Todo) {
